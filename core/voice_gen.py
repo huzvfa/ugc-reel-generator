f8773376ffa8da126c59883779747684690d4315
Huzaifa Baig <148705865+huzvfa@users.noreply.github.com>
2026-05-03 16:05:22 +0500

    Delete core/voice_gen.py

--- core/voice_gen.py
+++
@@ -1,6 +0,0 @@
-from gtts import gTTS
-
-def generate_voiceover(text, output_path="output/voice.mp3"):
-    tts = gTTS(text=text, lang='en', tld='us') # Clean US English accent
-    tts.save(output_path)
-    return output_path
